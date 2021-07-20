741a1b1c843081a3deb8e442bba09c8febb86883
dlscoccia <2054@holbertonschool.com>
2021-07-20 11:55:38 -0500

    Task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 '''python module'''
+import asyncio, random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    '''asyncrhonous coroutine'''
+    wait = random.uniform(0, max_delay)
+    await asyncio.sleep(wait)
+    return wait
--- 0x01-python_async_function/README.md
+++ 0x01-python_async_function/README.md
@@ -1,1 +1,3 @@
-readme
+# 0x01. Python - Async
+
+Asynchronous programming is a means of parallel programming in which a unit of work runs separately from the main application thread and notifies the calling thread of its completion, failure or progress.
