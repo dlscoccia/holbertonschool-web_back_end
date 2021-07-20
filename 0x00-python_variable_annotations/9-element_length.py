ad2a250907b200d61e3106834d420eac002f9d15
dlscoccia <2054@holbertonschool.com>
2021-07-19 21:47:45 -0500

    fixing pycodestyle

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,5 +2,7 @@
 '''python module'''
 from typing import Iterable, List, Tuple, Sequence
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''function'''
     return [(i, len(i)) for i in lst]
