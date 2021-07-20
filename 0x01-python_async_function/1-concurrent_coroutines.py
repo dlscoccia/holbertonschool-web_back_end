f08af08b7c30b44b9680028d3cbd007ffc8a82d8
dlscoccia <2054@holbertonschool.com>
2021-07-20 13:57:53 -0500

    Adding task 1, 2 & 3

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 '''python module'''
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''async func'''
+    delay_list = []
+    task_list = []
+    for x in range(n):
+        task = asyncio.create_task(wait_random(max_delay))
+        task_list.append(task)
+    for task in asyncio.as_completed(task_list):
+        value = await task
+        delay_list.append(value)
+    return delay_list
--- 0x01-python_async_function/README.md
+++ 0x01-python_async_function/README.md
@@ -1,3 +1,7 @@
 # 0x01. Python - Async
 
 Asynchronous programming is a means of parallel programming in which a unit of work runs separately from the main application thread and notifies the calling thread of its completion, failure or progress.
+
+### Event loop
+
+The event loop is the core of every asyncio application. Event loops run asynchronous tasks and callbacks, perform network IO operations, and run subprocesses.
