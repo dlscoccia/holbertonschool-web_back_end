17e5d3850cd709cd3ccd6ba9608e82ca5db0f268
dlscoccia <dlscoccia@gmail.com>
2021-08-11 19:41:37 -0500

    coockie

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -20,7 +20,7 @@ class SessionAuth(Auth):
 
         return session_id
 
-def user_id_for_session_id(self, session_id: str = None) -> str:
+    def user_id_for_session_id(self, session_id: str = None) -> str:
         """User ID"""
 
         if session_id is None or not isinstance(session_id, str):
