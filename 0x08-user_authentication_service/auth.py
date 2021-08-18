670895b59ff8f9c75f5baebd17b2881bfdf95e2d
dlscoccia <dlscoccia@gmail.com>
2021-08-17 20:26:04 -0500

    auth

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -2,6 +2,8 @@
 """ Authentication
 """
 from bcrypt import hashpw, gensalt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> str:
@@ -9,3 +11,18 @@ def _hash_password(password: str) -> str:
     Returns salted, hashed pswd as bytestring
     """
     return hashpw(password.encode('utf-8'), gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        """ Instance """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """new user if email isn't listed"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except Exception:
+            return self._db.add_user(email, _hash_password(password))
